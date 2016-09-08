f8860fdf1a128e606960209950fa176fe213667a
Cornelius Weidmann <cornelius@responsive.co.za>
2016-09-08 13:55:44 +0200

    Move logic into method.

--- panel.js
+++ panel.js
@@ -5,6 +5,41 @@
 **    chrome.extension.*
 */
 
+const container = document.querySelector('#storage-items');
+const messageContainer = document.querySelector('#message');
+
+const updateMarkup = function(storageItems) {
+  let markup = '';
+  let count = 0;
+
+  for (let key in storageItems) {
+    if (storageItems.hasOwnProperty(key)) {
+      let value = storageItems[key];
+      count++;
+
+      try {
+        item = JSON.parse(value);
+      } catch (err) {
+        item = value;
+      }
+
+      markup += `
+        <div class="storage-item ${count % 2 === 0 ? 'even' : 'odd'}">
+          <div class="key">${key}</div>
+          <div class="value">${value}</div>
+          <div class="clear"></div>
+        </div>
+      `;
+    }
+  }
+
+  if (markup) {
+    container.innerHTML = markup;
+  } else {
+    messageContainer.innerHTML = 'Local storage is empty.';
+  }
+};
+
 (function createChannel() {
   // Create a port with background page for continous message communication
   let port = chrome.extension.connect({
@@ -14,37 +49,7 @@
   // Listen to messages from the background page
   port.onMessage.addListener(function(message) {
     console.log('message', message);
-    let container = document.querySelector('#storage-items');
-
-    let markup = '';
-    for (let key in message.content) {
-      if (message.content.hasOwnProperty(key)) {
-        let value = message.content[key];
-        let item;
-
-        try {
-          item = JSON.parse(value);
-        } catch (err) {
-          item = value;
-        }
-
-        console.log('item', item);
-
-        markup += `
-          <div class="storage-item">
-            <div class="key">${key}</div>
-            <div class="value">${value}</div>
-            <div class="clear"></div>
-          </div>
-        `;
-      }
-    }
-
-    if (markup) {
-      container.innerHTML = markup;
-    } else {
-      container.innerHTML = 'localStorage is empty!';
-    }
+    updateMarkup(message.content);
   });
 })();
 
